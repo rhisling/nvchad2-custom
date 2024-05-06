a4717956fc14e4ebea9f9a30e130e8b92336aee0
aravindhan <aravindhansai@gmail.com>
2024-05-05 22:12:18 -0700

    add overseer and buil&run mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,27 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "v", "i", "t" }, "<C-b>", function()
+  require("nvchad.term").runner {
+    id = "RunAndBuildTerminal",
+    pos = "float",
+    cmd = function()
+      local file = vim.fn.expand "%" --returns absolute path 
+      local sfile = vim.fn.expand "%:r" -- returns absolute path without extension
+      local rfile = vim.fn.expand "%:~:." -- returns relative path
+      local rsfile = vim.fn.expand "%:~:.:r" -- returns relative path without extension
+      local ft_cmds = {
+        sh = "bash " .. file,
+        rust = "cargo " .. file,
+        python = "python3 " .. file,
+        javascript = "node " .. file,
+        go = "go build && go run " .. file,
+        c = "g++ " .. file .. " -o " .. sfile .. " && ./" .. sfile,
+        cpp = "clear && g++ -std=c++20 ./" .. rfile .. " -o " .. sfile .. " && ./" .. rsfile,
+        typescript = "deno compile " .. file .. " && deno run " .. file,
+      }
+      return ft_cmds[vim.bo.ft]
+    end,
+  }
+end, { desc = "Build and Run file" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,23 +8,46 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
   --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",   --lua
+        "html-lsp", "css-lsp", "prettier", --html, css
+        "clangd", "clang-format",          --cpp
+        "gopls",                           --go
+        "pyright",                         --python
+        "rust-analyzer"                    --rust
+      },
+    },
+  },
+  { -- This plugin
+    "Zeioth/compiler.nvim",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "stevearc/overseer.nvim" },
+    opts = {},
+  },
+  { -- The task runner we use
+    "stevearc/overseer.nvim",
+    commit = "68a2d344cea4a2e11acfb5690dc8ecd1a1ec0ce0",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1
+      },
+    },
+  },
   --
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
