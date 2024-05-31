dfd5d67fa0f3751fdc9e4a64c79880d36bd3c51a
aelayaku <aelayaku@cisco.com>
2024-05-30 23:08:47 -0700

    add go,js and py formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,18 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    go = { "goimports", "gofmt" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { { "prettierd", "prettier" } },
+    -- You can use a function here to determine the formatters dynamically
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
@@ -8,6 +20,12 @@ local options = {
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  -- If this is set, Conform will run the formatter asynchronously after save.
+  -- It will pass the table to conform.format().
+  -- This can also be a function that returns the table.
+  format_after_save = {
     lsp_fallback = true,
   },
 }
