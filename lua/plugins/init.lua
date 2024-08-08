3b644d8da356611d6272bec8fce4e575bc060d50
aravindhan <aravindhansai@gmail.com>
2024-08-08 02:21:19 -0700

    fix conform formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,8 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    event = "BufWritePre", -- uncomment for format on save 
+    opts = require "configs.conform", 
   },
 
   -- These are some examples, uncomment them if you want to see them work!
