1504fb6eb0e480d625805432242e96cbc636589d
aravindhan <aravindhansai@gmail.com>
2024-08-29 01:15:42 -0700

    add neo-tree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -63,7 +63,24 @@ return {
         "go",
         "rust",
         "python",
+        "zig",
       },
     },
   },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --     "MunifTanjim/nui.nvim",
+  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --   },
+  --   config = require("configs.neotree").configFn,
+  --   keys = {
+  --     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+  --   },
+  --   cmd = { "Neotree" },
+  --   event = { "VeryLazy", "BufEnter" },
+  -- },
 }
