a466add3ee5c94d9cf14064841590ab3902d61d2
aelayaku <aelayaku@cisco.com>
2024-09-09 09:58:15 -0700

    update deps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -15,7 +15,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
